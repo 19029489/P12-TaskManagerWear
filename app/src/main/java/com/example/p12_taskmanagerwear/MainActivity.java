43a95feaf3204577c3cf1c8390ae44f34c5048d0
19029489 <19029489@rp.edu.sg>
2021-08-05 17:00:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/p12_taskmanagerwear/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.p12_taskmanagerwear;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.RemoteInput;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayList<Task> al;
+    ArrayAdapter<Task> aa;
+    Button btnAdd;
+    DBHelper db = new DBHelper(MainActivity.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.lvTask);
+        btnAdd = findViewById(R.id.buttonAdd);
+
+        al = new ArrayList<Task>();
+
+        al = db.getAllTasks();
+
+        aa = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+//        CharSequence reply = null;
+//        Intent intent = getIntent();
+//
+//        long id = intent.getLongExtra("id", 0);
+//
+//        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+//        if (remoteInput != null) {
+//            reply = remoteInput.getCharSequence("status");
+//        }
+//
+//        if (reply != null && reply.toString().equalsIgnoreCase("Completed")) {
+//
+//            db.deleteTask(id);
+//            al.clear();
+//            al = db.getAllTasks();
+//            aa.notifyDataSetChanged();
+//        }
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        CharSequence reply = null;
+        Intent intent = getIntent();
+
+        long id = intent.getLongExtra("id", 0);
+        Log.d("id:", id+"");
+
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            reply = remoteInput.getCharSequence("status");
+        }
+
+        if (reply != null && reply.toString().equalsIgnoreCase("Completed")) {
+
+            db.deleteTask(id);
+            al.clear();
+            al = db.getAllTasks();
+            aa.notifyDataSetChanged();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/p12_taskmanagerwear/ScheduledNotificationReceiver.java
@@ -0,0 +1,97 @@
+package com.example.p12_taskmanagerwear;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
+
+import java.util.ArrayList;
+
+public class ScheduledNotificationReceiver extends BroadcastReceiver {
+    ArrayList<Task> al = new ArrayList<Task>();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        long id = intent.getLongExtra("id", 0);
+        String task_name = intent.getStringExtra("task_name");
+        String task_desc = intent.getStringExtra("task_desc");
+
+        Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("This is for default notification");
+
+            channel.enableVibration(true);
+            channel.enableLights(true);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent i = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, Integer.parseInt("" + id), i, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.mipmap.ic_launcher, "Launch Task Manager", pIntent).build();
+
+        Intent intentreply = new Intent(context, MainActivity.class);
+        intent.putExtra("id", id);
+        PendingIntent pendingIntentReply = PendingIntent.getActivity(context, 0, intentreply, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        RemoteInput ri = new RemoteInput.Builder("status")
+                .setLabel("Status report")
+                .setChoices(new String[] {"Completed", "Not yet"})
+                .build();
+
+        NotificationCompat.Action action2 = new NotificationCompat.Action.Builder(R.mipmap.ic_launcher, "Reply", pendingIntentReply)
+                .addRemoteInput(ri)
+                .build();
+
+        NotificationCompat.WearableExtender extender = new NotificationCompat.WearableExtender();
+        extender.addAction(action);
+        extender.addAction(action2);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), android.R.drawable.ic_dialog_info);
+
+        // build notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        builder.setContentTitle("Task");
+        builder.setContentText(task_name + "\n" + task_desc);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+//        builder.setLargeIcon(largeIcon);
+//        builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(largeIcon).bigLargeIcon(null));
+        builder.setAutoCancel(true);
+
+        builder.extend(extender);
+
+        //set vibrate
+        builder.setVibrate(new long[] { 500, 500 });
+
+        //LED lights
+        builder.setLights(Color.RED, 3000, 3000);
+
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(uri);
+
+        builder.setPriority(Notification.PRIORITY_HIGH);
+
+        Notification n = builder.build();
+        notificationManager.notify(Integer.parseInt("" + id), n);
+
+    }
+}
